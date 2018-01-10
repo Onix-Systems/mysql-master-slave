4202bac24879bcc800040de8949b9b6b37b11b3f
Nikolay Dikhtyar <nikolay.dikhtyar@onix-systems.com>
2018-01-10 13:39:37 +0200

    Fixed MySQL Slave server starting

--- scripts/configurator.sh
+++ scripts/configurator.sh
@@ -2,15 +2,25 @@
 
 set -e
 
-[ -z "${DATA_FOLDER}" ] && DATA_FOLDER=/data
+[ -z "${MASTER_DATA_FOLDER}" ] && MASTER_DATA_FOLDER=/data/master
+[ -z "${SLAVE_DATA_FOLDER}" ] && MASTER_DATA_FOLDER=/data/slave
 [ -z "${MYSQL_USER}" ] && MYSQL_USER=mysql
 
 MYSQL_USER_ID=$(id -u ${MYSQL_USER})
 MYSQL_GROUP_ID=$(id -g ${MYSQL_USER})
 
-printf "Checking rights for data folder. "
-if [ "$(stat -c %u:%g ${DATA_FOLDER})" != "${MYSQL_USER_ID}:${MYSQL_GROUP_ID}" ]; then
-    chown -R ${MYSQL_USER_ID}:${MYSQL_GROUP_ID} ${DATA_FOLDER}
+printf "Checking rights for master data folder. "
+if [ "$(stat -c %u:%g ${MASTER_DATA_FOLDER})" != "${MYSQL_USER_ID}:${MYSQL_GROUP_ID}" ]; then
+    chown -R ${MYSQL_USER_ID}:${MYSQL_GROUP_ID} ${MASTER_DATA_FOLDER}
+    echo "Done."
+else
+    echo "No changes."
+fi
+echo
+
+printf "Checking rights for slave data folder. "
+if [ "$(stat -c %u:%g ${SLAVE_DATA_FOLDER})" != "${MYSQL_USER_ID}:${MYSQL_GROUP_ID}" ]; then
+    chown -R ${MYSQL_USER_ID}:${MYSQL_GROUP_ID} ${SLAVE_DATA_FOLDER}
     echo "Done."
 else
     echo "No changes."
--- scripts/slave.sh
+++ scripts/slave.sh
@@ -2,8 +2,9 @@
 
 set -e
 
+MAX_WAIT_TIMEOUT=30
+
 [ -z "${MASTER_DB_HOST}" ] && MASTER_DB_HOST=master
-MASTER_DB_PORT=3306
 [ -z "${MYSQL_ROOT_USER}" ] && MYSQL_ROOT_USER=root
 [ -z "${MYSQL_ROOT_PASSWORD}" ] && MYSQL_ROOT_PASSWORD=empty
 
@@ -12,14 +13,25 @@ if [ -z "${SLAVE_USER}" ] || [ -z "${SLAVE_USER_PASSWORD}" ]; then
     exit 1
 fi
 
-printf "Waiting for master DB will be ready."
+printf "Waiting for master DB will be ready"
 IS_READY=false
+I=0
+set +e
 while [ $IS_READY == false ]; do
-    sleep 10
-    nc -z -w 1 ${MASTER_DB_HOST} ${MASTER_DB_PORT}
+    nc -z -w 1 ${MASTER_DB_HOST} 3306
     [ $? == 0 ] && IS_READY=true
+    printf "."
+    sleep 1
+    let I=${I}+1
+    if [ ${I} -eq ${MAX_WAIT_TIMEOUT} ]; then
+        echo
+        echo "Max wait timeout of waiting for MySQL ready state was exceeded."
+        exit 1
+    fi
 done
-echo
+sleep 5
+echo " Ready."
+set -e
 
 echo "Preparing master db host to be connected by slave replica."
 export MYSQL_PWD=${MYSQL_ROOT_PASSWORD}
@@ -33,6 +45,8 @@ STATUS=$(mysql -sN -uroot -h${MASTER_DB_HOST} -e "SHOW MASTER STATUS;")
 LOG_FILE=$(echo ${STATUS} | cut -f1 -d " ")
 POSITION=$(echo ${STATUS} | cut -f2 -d " ")
 
+echo "${LOG_FILE} / ${POSITION}"
+
 printf "Downloading database dump. "
 mysqldump -uroot -h${MASTER_DB_HOST} ${MYSQL_DATABASE} > /tmp/${MYSQL_DATABASE}.sql
 echo "Done."
@@ -40,8 +54,7 @@ echo "Done."
 mysql -uroot -h${MASTER_DB_HOST} ${MYSQL_DATABASE} -e "UNLOCK TABLES; SHOW MASTER STATUS;"
 
 # Working on slave node
-mysql -uroot -hlocalhost ${MYSQL_DATABASE} < /tmp/${MYSQL_DATABASE}.sql
-
+mysql -uroot ${MYSQL_DATABASE} < /tmp/${MYSQL_DATABASE}.sql
 mysql -uroot -e "CHANGE MASTER TO MASTER_HOST='${MASTER_DB_HOST}',MASTER_USER='${SLAVE_USER}', MASTER_PASSWORD='${SLAVE_USER_PASSWORD}', MASTER_LOG_FILE='${LOG_FILE}', MASTER_LOG_POS=${POSITION};"
 
 if [ -z "${CUSTOM_CONF_TEMPLATE}" ] || [ -z "${CUSTOM_CONF}" ]; then
