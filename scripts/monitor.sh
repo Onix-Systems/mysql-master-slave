3753e7a70e9f9e8dd121133d2900a8ed604f86d3
Nikolay Dikhtyar <nikolay.dikhtyar@onix-systems.com>
2018-01-11 21:35:05 +0200

    Fixed creating monitor user for proxysql service

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 echo "Creating monitor user for proxysql"
 
@@ -8,7 +8,12 @@ export MYSQL_PWD=${MYSQL_ROOT_PASSWORD}
 # TODO: Clarify more correct rights for monitor user
 #
 
+if [ -z "${MONITOR_USERNAME}" ] || [ -z "${MONITOR_PASSWORD}" ]; then
+    echo "MONITOR_USERNAME and MONITOR_PASSWORD must be defined. Please check."
+    exit 1
+fi
+
 mysql -uroot -e "
-    CREATE USER '${MONITOR_USER}'@'%' IDENTIFIED BY '${MONITOR_PASSWORD}';
-    GRANT ALL ON *.* TO '${MONITOR_USER}'@'%';
+    CREATE USER '${MONITOR_USERNAME}'@'%' IDENTIFIED BY '${MONITOR_PASSWORD}';
+    GRANT ALL ON *.* TO '${MONITOR_USERNAME}'@'%';
 "
